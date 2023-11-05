3320d74c4bce42183265ac7b050f2df91fb2b1ae
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2023-11-05 11:37:07 -0800

    ...

--- BackEnd/handler/storeKeeper/storeKeeperBlessing.js
+++ BackEnd/handler/storeKeeper/storeKeeperBlessing.js
@@ -1,3 +1,3 @@
 //check if the the request with the given id exists in the StoreHead approved Employee request DB
 //if it exists check if the confirmation number of  the given request match with id of the stored request in the DB
-//if it match insert the data with the additional parameter of ----> (storeKeeper user_name ) into the History DB.
+//if it match , insert the data with the additional parameter of ----> (storeKeeper user_name ) into the History DB.
