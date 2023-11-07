09ca010f394aa30a9ab3e8f64ec1071a4fbf6b18
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2023-11-07 11:38:52 -0800

    ...

--- BackEnd/utility/common/addNewUser.js
+++ BackEnd/utility/common/addNewUser.js
@@ -12,7 +12,6 @@ const NewUser=gql`
     mutation MyMutation($user_name:String!, $password:String!, $first_name:String!, $last_name:String!, $role_id:Int!){
         insert_User_one(object: {user_name: $user_name, Password: $password, first_name: $first_name, last_name: $last_name, role_id: $role_id}) {
         user_name
-        Password
         first_name
         last_name
         Role {
