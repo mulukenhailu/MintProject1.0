98f099874cea68bb4f78f9c414193fccb882b8c8
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2024-01-17 11:30:09 -0800

    some fix

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -28,12 +28,8 @@ const deleteItem = require("./handler/item/deleteItem");
 
 const createItem = require("./handler/item/createItem");
 
-<<<<<<< HEAD
 const getAllUser = require("./handler/common/getAllUser");
 const managerMakeRequest=require("./handler/manager/makeRequest/makeRequest")
-=======
-const managerMakeRequest = require("./handler/manager/makeRequest/makeRequest");
->>>>>>> d20ad6937c1d8b26f92d5e73c74147926187b423
 const allManagersAvailable = require("./handler/manager/allManageravailable");
 const manageracceptance = require("./handler/manager/acceptedRequest/acceptedRequest");
 const managerOwnRequest=require("./handler/manager/ownPastRequest/ownPastRequest")
