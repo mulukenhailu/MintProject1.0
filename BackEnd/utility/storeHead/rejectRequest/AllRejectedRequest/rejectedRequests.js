4f70949d627b454fa99eaa8ec7bbebf8052af7c6
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2024-01-17 16:05:08 -0800

    some params modification

--- BackEnd/utility/storeHead/rejectRequest/AllRejectedRequest/rejectedRequests.js
+++ BackEnd/utility/storeHead/rejectRequest/AllRejectedRequest/rejectedRequests.js
@@ -8,53 +8,104 @@ const client = new GraphQLClient(endpoint, {
     },
   })
 
+  // const doc=gql`
+  // query MyQuery {
+  //   ManagerAppEmpRequest(where: 
+  //     {isApprovedByManager: {_eq: true}, 
+  //     isApprovedByStoreHead: {_eq: false}, 
+  //     isRejectedByManager: {_eq: false}, 
+  //     isRejectedByStoreHead: {_eq: true}}) {
+  //     employee_username
+  //     id
+  //     is_approved
+  //     item_name
+  //     item_no
+  //     manager_username
+  //     quantity_requested
+  //     storehead_username
+  //     employeeRequest {
+  //       created_at
+  //       employee_username
+  //       quantity_requested
+  //       Item {
+  //         created_at
+  //         item_number
+  //         productdescription
+  //         productmodel
+  //         productmodelnumber
+  //         productname
+  //         productphoto
+  //         productquantitynumber
+  //         productsource
+  //         productstandardtype
+  //         productstatus
+  //         updated_at
+  //         request {
+  //           User {
+  //             created_at
+  //             department
+  //             email
+  //             first_name
+  //             last_name
+  //             manager_username
+  //             phone_number
+  //             profile_picture
+  //             user_name
+  //             updated_at
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  // `
+
   const doc=gql`
   query MyQuery {
-    ManagerAppEmpRequest(where: {isApprovedByManager: {_eq: true}, isApprovedByStoreHead: {_eq: false}, isRejectedByManager: {_eq: false}, isRejectedByStoreHead: {_eq: true}}) {
-      employee_username
+    ManagerAndEmpRequest(where: {_and: {isApprovedByManager: {_eq: true}, isApprovedByStoreHead: {_eq: false}, isRejectedByManager: {_eq: false}, isRejectedByStoreHead: {_eq: true}}}) {
       id
-      is_approved
-      item_name
       item_no
-      manager_username
+      item_name
       quantity_requested
+      manager_username
+      employee_username
       storehead_username
-      employeeRequest {
+      confirmation_number
+      isApprovedByManager
+      isApprovedByStoreHead
+      isRejectedByManager
+      isRejectedByStoreHead
+      is_approved
+      created_at
+      updated_at
+      Item {
+        item_number
+        productdescription
+        productmodel
+        productname
+        productphoto
+        productquantitynumber
+        productsource
+        productstandardtype
+        productstatus
+        productmodelnumber
         created_at
-        employee_username
-        quantity_requested
-        Item {
-          created_at
-          item_number
-          productdescription
-          productmodel
-          productmodelnumber
-          productname
-          productphoto
-          productquantitynumber
-          productsource
-          productstandardtype
-          productstatus
-          updated_at
-          request {
-            User {
-              created_at
-              department
-              email
-              first_name
-              last_name
-              manager_username
-              phone_number
-              profile_picture
-              user_name
-              updated_at
-            }
-          }
-        }
+        updated_at
+      }
+      User {
+        user_name
+        first_name
+        last_name
+        email
+        department
+        manager_username
+        phone_number
+        profile_picture
+        created_at
+        updated_at
       }
     }
   }
-  
   
   `
 
