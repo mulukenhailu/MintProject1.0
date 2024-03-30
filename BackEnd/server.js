045d0310e92ac1fbaaec17f852aa98cdb7b610d1
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2024-03-29 20:43:25 -0700

    add more cors option

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -63,6 +63,7 @@ var whitelist = [
   "http://localhost:3000",
   "https://mint-s0j6.onrender.com",
   "http://localhost:3001/uploads",
+  "https://mint-private-deploy.vercel.app/"
 ];
 
 const corsOptions = {
